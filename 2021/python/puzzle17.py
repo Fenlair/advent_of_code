3191c06b40a35e802e0b963edaa2805a8de7e321
Pascal Hertrich <fenlair@outlook.com>
2021-12-19 12:40:55 +0100

    completed day17

--- 2021/python/puzzle17.py
+++ 2021/python/puzzle17.py
@@ -1,9 +1,13 @@
 import math
+import re
 
 example = "target area: x=20..30, y=-10..-5"
 real = "target area: x=144..178, y=-100..-76"
 
-target = ((20, 30), (-10, -5))
+def parse(data):
+    m = re.search(r"x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)", data)
+    xa, xb, ya, yb = m.groups()
+    return ((int(xa), int(xb)), (int(ya), int(yb)))
 
 def step(p, v):
     p = [p[0] + v[0], p[1] + v[1]]
@@ -12,33 +16,42 @@ def step(p, v):
     return p, v
 
 def is_in_target(p):
-    x = target[0][0] <= p[0] <= target[0][1]
-    y = target[1][0] <= p[1] <= target[1][1]
+    x = min(target[0]) <= p[0] <= max(target[0])
+    y = min(target[1]) <= p[1] <= max(target[1])
     return x and y
 
 def calc_x_vel(target):
-    t = target[0][0]
+    """Calculate minimum velocity to reach target."""
+    t = min(target[0])
     v = math.ceil(-1/2 + math.sqrt(1/4 + 2*t))
-    assert v <= target[0][1]
+    assert (v+1)/2*v <= max(target[0])
     return v
-
 
 def throw(v):
     global record, made_it
     v_orig = tuple(v)
     p = [0, 0]
-    for _ in range(100):
+    while True:
         p, v = step(p, v)
-        if p[1] >= record.get(v_orig, -10):
+        if p[0] > max(target[0]):
+            break
+        if v[1] <= 0 and p[1] < min(target[1]):
+            break
+        if p[1] >= record.get(v_orig, -1000):
             record[v_orig] = p[1]
         if is_in_target(p):
             made_it.add(v_orig)
             break
 
+target = parse(real)
 record = {}
 made_it = set()
-for y in range(20):
-    throw([calc_x_vel(target), y])
+
+for y in range(min(target[1]), 200):
+    for x in range(calc_x_vel(target), max(target[0])+1):
+        throw([x, y])
+
 record = {k: v for k, v in record.items() if k in made_it}
 m = max(record, key=record.get)
-print(m, record[m])
+print(f'Puzzle1: {record[m]} for initial velocity of {m}')
+print(f'Puzzle2: {len(made_it)}')
